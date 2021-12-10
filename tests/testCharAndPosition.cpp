d97710d6371c88acc1f9e6dded78e7bb21ff3396
Łukasz Reszka <01143598@pw.edu.pl>
2021-12-10 15:48:46 +0100

    Fixed old test suites

--- tests/testCharAndPosition.cpp
+++ tests/testCharAndPosition.cpp
@@ -9,15 +9,15 @@ TEST_CASE("constructors tests") {
     SUBCASE("not EOF constructor") {
         code_source::CharAndPosition test_struct('a', 1, 2);
         CHECK_EQ('a', test_struct.value);
-        CHECK_EQ(1, test_struct.line);
-        CHECK_EQ(2, test_struct.column);
+        CHECK_EQ(1, test_struct.position.line);
+        CHECK_EQ(2, test_struct.position.column);
         CHECK_EQ(false, test_struct.isEndOfText);
     }
 
     SUBCASE("EOF constructor") {
         code_source::CharAndPosition test_struct(3, 4);
-        CHECK_EQ(3, test_struct.line);
-        CHECK_EQ(4, test_struct.column);
+        CHECK_EQ(3, test_struct.position.line);
+        CHECK_EQ(4, test_struct.position.column);
         CHECK_EQ(true, test_struct.isEndOfText);
     }
 }
--- tests/testCodeSource.cpp
+++ tests/testCodeSource.cpp
@@ -1,4 +1,5 @@
 #include <sstream>
+#include <fstream>
 #include "../libs/doctest/include/doctest.h"
 #include "../src/codeSource/codeSource.h"
 
@@ -10,20 +11,22 @@ TEST_CASE("getNextChar() method test - code from file") {
     std::fstream source_file;
     std::string file_name = "test_source.txt";
 
-    source_file.open(file_name, std::ios::out);
+    REQUIRE_NOTHROW(source_file.open(file_name, std::ios::out));
     REQUIRE_UNARY_FALSE(source_file.fail());
     std::string code = "ab ; \n x\n\n";
     source_file << code;
     source_file.close();
 
-    code_source::CodeSource *source;
-    REQUIRE_NOTHROW(source = new code_source::CodeSource(file_name));
+    REQUIRE_NOTHROW(source_file.open(file_name, std::ios::in));
+    REQUIRE_UNARY_FALSE(source_file.fail());
+
+    code_source::CodeSource source(source_file);
 
     for (int i = 0; i < 10; ++i) {
-        code_source::CharAndPosition singleChar = source->getNextChar();
+        code_source::CharAndPosition singleChar = source.getNextChar();
         CHECK_EQ(code[i], singleChar.value);
-        CHECK_EQ(lin, singleChar.line);
-        CHECK_EQ(col, singleChar.column);
+        CHECK_EQ(lin, singleChar.position.line);
+        CHECK_EQ(col, singleChar.position.column);
         CHECK_EQ(false, singleChar.isEndOfText);
         if (code[i] == '\n') {
             col = 1;
@@ -33,43 +36,37 @@ TEST_CASE("getNextChar() method test - code from file") {
         }
     }
 
-    code_source::CharAndPosition singleChar = source->getNextChar();
-    CHECK_EQ(4, singleChar.line);
-    CHECK_EQ(1, singleChar.column);
+    code_source::CharAndPosition singleChar = source.getNextChar();
+    CHECK_EQ(4, singleChar.position.line);
+    CHECK_EQ(1, singleChar.position.column);
     CHECK_EQ(true, singleChar.isEndOfText);
 
-    delete source;
+    source_file.close();
     remove(file_name.c_str());
 }
 
-TEST_CASE("getNextChar() method test - code from terminal") {
+TEST_CASE("getNextChar() method test - code from string stream") {
     int lin = 1, col = 1;
-    std::string code = "ab ; \n x^q\n\n^q";
-    std::stringstream user_input_mock(code);
-    code_source::CodeSource source(user_input_mock);
+    std::string code = "ab ; \n x\n\n";
+    std::stringstream input(code);
+    code_source::CodeSource source(input);
     for (int i = 0; i < 10; ++i) {
         code_source::CharAndPosition singleChar = source.getNextChar();
         CHECK_EQ(code[i], singleChar.value);
-        CHECK_EQ(lin, singleChar.line);
-        CHECK_EQ(col, singleChar.column);
+        CHECK_EQ(lin, singleChar.position.line);
+        CHECK_EQ(col, singleChar.position.column);
         CHECK_EQ(false, singleChar.isEndOfText);
-        if (i == 4) {
+        if (code[i] == '\n') {
             col = 1;
             ++lin;
-            ++i;
         } else {
             ++col;
         }
     }
-    code_source::CharAndPosition singleChar = source.getNextChar();
-    CHECK_EQ('\0', singleChar.value);
-    CHECK_EQ(3, singleChar.line);
-    CHECK_EQ(1, singleChar.column);
-    CHECK_EQ(false, singleChar.isEndOfText);
 
-    singleChar = source.getNextChar();
-    CHECK_EQ(4, singleChar.line);
-    CHECK_EQ(1, singleChar.column);
+    code_source::CharAndPosition singleChar = source.getNextChar();
+    CHECK_EQ(4, singleChar.position.line);
+    CHECK_EQ(1, singleChar.position.column);
     CHECK_EQ(true, singleChar.isEndOfText);
 }
 
